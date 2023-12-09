316eb917709356dd6052a1cc8de8867577baa6ea
piocie <piocie87@gmail.com>
2023-12-09 22:57:57 +0000

    Task 1 completed

---
+++ T01/hello_world.py
@@ -0,0 +1,13 @@
+# get a name from user and store it in name variable
+# display the user name
+# get an age from user and store it in age variable
+# display out the user age
+# display the message "Hello World!"
+
+name = input("What is your name ? ")
+print(name)
+print()
+age = input("What is your age ? ")
+print(age)
+print()
+print("Hello World!")
