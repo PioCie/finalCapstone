344be97f06ab5ac6b34abf4f7d6b0dad11206c58
piocie <piocie87@gmail.com>
2023-12-09 23:01:21 +0000

    Tasks 2 & 3 completed

---
+++ T01/conversion.py
@@ -0,0 +1,18 @@
+# declare variable num1 with value 99.23
+# declare variable num2 with value 23
+# declare variable num3 with value 150
+# declare variable string1 with value 100
+# convert into an integer and display variable num1
+# convert into a float and display variable num2
+# convert into a string and display variable num3
+# convert into an integer and display variable string1
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
---
+++ T01/details.py
@@ -0,0 +1,12 @@
+# get a name from user and store it in name variable
+# get an age from user and store it in age variable
+# get a house number from user and store it in house_number variable
+# get a street name from user and store it in street_name variable
+# display the sentence with specific variables
+
+name = input("What is your name ? ")
+age = input("What is your age ? ")
+house_number = input("What is your house number ? ")
+street_name = input("What is your street name ? ")
+
+print(f"This is {name}. He is {age} years old and lives at house number {house_number} on {street_name} street.")
