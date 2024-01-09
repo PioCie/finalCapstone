a00aa5a75909b3262722e4123101e2f5694be81d
piocie <piocie87@gmail.com>
2024-01-09 19:56:03 +0000

    Practical Task completed

--- T14/dob_task.py
+++ T14/dob_task.py
@@ -1,5 +1,23 @@
-with open('DOB.txt', 'r+') as file:
-    content = file.readlines()
-    for i in range(len(content)):
-        print(type(i))
+# Open the file for reading
+with open("DOB.txt", "r", encoding="utf-8") as file:
+    # Initialize lists to store names and birthdays
+    names = []
+    birthdays = []
+    # Read each line from the file
+    for line in file:
+        # Split the line into name, surname and birthdate
+        name, surname, birthdate = line.strip().split(" ", 2)
+        # Added name and surname to creat a full name
+        full_name = name + " " + surname
+        # Append the name and birthdate to their respective lists
+        names.append(full_name)
+        birthdays.append(birthdate)
+
+# Print the names section
+print("Name:")
+print("\n".join(names))
+print()
+# Print the birthdays section
+print("Birthdate:")
+print("\n".join(birthdays))
 
