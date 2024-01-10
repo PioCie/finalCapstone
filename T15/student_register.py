52bfcc64353670769a42a7f42ef1385d21d3934e
piocie <piocie87@gmail.com>
2024-01-10 05:31:21 +0000

    Practical Task completed

--- T15/student_register.py
+++ T15/student_register.py
@@ -5,6 +5,6 @@ for student in range(1, int(num_of_students)+1):
     # Asked user to enter a ech student ID
     student_id = input("Please provide a student ID: ")
     # Saves the student ID to a file and added dotted line for student signature
-    with open("reg_form", "a") as file:
+    with open("reg_form.txt", "a") as file:
         file.write(student_id+"\n"+"\n")
         file.write(25*"."+"\n"+"\n")
