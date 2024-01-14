051edd482d4804766c2ff59081edf49362fc008e
piocie <piocie87@gmail.com>
2024-01-14 21:03:47 +0000

    Finalisation last functions

--- T16/holiday.py
+++ T16/holiday.py
@@ -1,41 +1,41 @@
-# Created hotel_cost function
+# Defined functions hotel_cost
 def hotel_cost(num_days, rate):
     return num_days * rate
 
 
-# Created plane_cost function
+# Defined functions plane_cost
 def plane_cost(city):
-    if city == "Madrid":
+    if city == "madrid":
         price = 150
         return price
-    elif city == "Krakow":
+    elif city == "krakow":
         price = 200
         return price
-    elif city == "Lisbon":
+    elif city == "lisbon":
         price = 220
         return price
-    elif city == "Paris":
+    elif city == "paris":
         price = 300
         return price
-    elif city == "Rome":
+    elif city == "rome":
         price = 280
         return price
-    elif city == "Berlin":
+    elif city == "berlin":
         price = 270
         return price
-    elif city == "Prague":
+    elif city == "prague":
         price = 240
         return price
     else:
         print("Unable to find destination, please check your spelling")
 
 
-# Created plane_cost function
+# Defined functions car_rental
 def car_rental(num_days):
     return num_days * 22
 
 
-# Created plane_cost function
+# Defined functions holiday_cost
 def holiday_cost(hotel, plane, car):
     return hotel + plane + car
 
@@ -46,6 +46,10 @@ print("Welcome to ..::Holiday costs calculator::..\n")
 print("Where would you like to go for your city break ??\n")
 print("You can choose one of the fallowing destination:\n")
 print("Madrid, Krakow, Lisbon, Paris, Rome, Berlin, Prague\n")
-city_flight = input("Please choose your flight destination: ")
+city_flight = input("Please choose your flight destination: ").lower()
 num_nights = input("How many nights would you like to stay: ")
 rental_day = input("For how many days would you like hire a car: ")
+print()
+print("Great choose, It will be great holiday")
+print(f"The flight price to your destination is {plane_cost(city_flight)}£.")
+
