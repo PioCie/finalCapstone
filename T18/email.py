fa71db1da25960b8071920f809a88299eda11687
piocie <piocie87@gmail.com>
2024-02-16 20:49:36 +0000

    Started Practical Task

---
+++ T18/email.py
@@ -0,0 +1,6 @@
+class Email:
+    def __init__(self, email_address, subject_line, email_content):
+        self.email_address = email_address
+        self.subject_line = subject_line
+        self.email_content = email_content
+        has_been_read
